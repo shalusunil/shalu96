91bb18a31d5bbe33514de791780badb99a0d2c97
shalusunil <shalusunil96@gmail.com>
2017-06-29 15:37:43 +0530

    Create program3.c

---
+++ program3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+printf("Number is positive");
+else if(a<0)
+printf("Number is negative");
+else
+printf("Number is zero");
+return o;
+}
