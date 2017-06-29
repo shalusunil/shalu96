e2cd9be7e1915d80fe244fb96939ab80987f6d3d
shalusunil <shalusunil96@gmail.com>
2017-06-29 11:53:43 +0530

    Create program1.c

---
+++ program1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("number is even",a);
+}
+else
+{
+printf("number is odd",a);
+}
+getch();
+}
