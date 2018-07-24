83d95b98c7fa9e44fc556c751a3f746091fba4d7
shalusunil <shalusunil96@gmail.com>
2018-07-24 14:17:02 +0530

    Create median.c

---
+++ cprogramming/median.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,j,n,t;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+	    for(j=i+1;j<n;j++)
+	    {
+	        if(a[i]>a[j])
+	        {
+	        t=a[i];
+	        a[i]=a[j];
+	        a[j]=t;
+	        }
+	}
+	}
+	printf("%d\n",a[n/2]);
+	return 0;
+}
